273bb2075db2a5e90a1064d058428f035a2365ca
Hokuto Shinoda <isucon@ip-172-31-21-18.ap-northeast-1.compute.internal>
2017-07-09 12:05:35 +0000

    Revert "Revert "fix(php): last loginをredisに載せようと思ったけど、既存ログのmigrationが上手く行っていないのか、mypageでexpected ip mismatchで怒られる'""
    This reverts commit 2eaa058923c9daa227f3fb72edbbb44ccd1e56af.

--- php/src/index.php
+++ php/src/index.php
@@ -67,6 +67,18 @@ function login_log($succeeded, $login, $user_id=null) {
       $redis->hincrby('LoginFailuresByIp', $_SERVER['REMOTE_ADDR'], 1);
   }
 
+  if ($succeeded) {
+      $redis = option('redis');
+      $redis->hset(
+          'LoginSuccessLog',
+          $user_id,
+          serialize([
+              (new DateTime())->format('Y-m-d H:i:s'),
+              $_SERVER['REMOTE_ADDR']
+          ])
+      );
+  }
+
   $stmt = $db->prepare('INSERT INTO login_log (`created_at`, `user_id`, `login`, `ip`, `succeeded`) VALUES (NOW(),:user_id,:login,:ip,:succeeded)');
   $stmt->bindValue(':user_id', $user_id);
   $stmt->bindValue(':login', $login);
@@ -131,19 +143,21 @@ function current_user() {
   return isset($_SESSION['user']) ? $_SESSION['user'] : null;
 }
 
-function last_login() {
-  $user = current_user();
-  if (empty($user)) {
-    return null;
+function last_login($userId) {
+  $redis = option('redis');
+
+  $log = unserialize($redis->hget('LoginSuccessLog', $userId));
+  if (!$log) {
+      return [
+          'created_at' => null,
+          'ip'         => null
+      ];
   }
 
-  $db = option('db_conn');
-
-  $stmt = $db->prepare('SELECT * FROM login_log WHERE succeeded = 1 AND user_id = :id ORDER BY id DESC LIMIT 2');
-  $stmt->bindValue(':id', $user['id']);
-  $stmt->execute();
-  $stmt->fetch();
-  return $stmt->fetch(PDO::FETCH_ASSOC);
+  return [
+    'created_at' => $log[0],
+    'ip'         => $log[1],
+  ];
 }
 
 function banned_ips() {
@@ -214,7 +228,7 @@ dispatch_get('/mypage', function() {
   }
   else {
     set('user', $user);
-    set('last_login', last_login());
+    set('last_login', last_login($user['id']));
     return html('mypage.html.php');
   }
 });
